aa982e1c133f4c130f8657ec7082618d6a2b22e6
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-07-22 22:02:09 +0300

    using fonction, while, and if

--- my_app.py
+++ my_app.py
@@ -1,2 +1,18 @@
-name = input('what is your name?')
-print('hello ' + name)
+def deposit():
+    while True:
+        amount = input("what would you like to deposit? $")
+        if amount.isdigit():
+           amount=int(amount)
+           if amount > 0:
+              break
+           else:
+              print("Amount must be greater than 0.")
+        else:
+            print("please enter a number")
+
+    return amount     
+
+def main():
+        balance = deposit()
+
+main()
