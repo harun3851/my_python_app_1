93cc180774f2695e6227db385354b9ca4f594ee3
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-07-24 20:19:19 +0300

    using string

---
+++ string_1.py
@@ -0,0 +1,28 @@
+#print a 
+a="harun"
+print(a)
+
+#print multiline
+b="""hello world
+how are you
+how is life"""
+print(b)
+
+#print character
+a="hello world"
+print(a[0])
+
+#loop through the letters 
+for c in "banana":
+    print(c)
+#check to string line
+a="hello  world"
+print(len(a))
+#check to string
+txt="the best thing"
+print("the" in txt)
+
+txt="the best thing in life is free"
+if "free" in txt:
+    print("yes,'free' is present")
+    print("expensive" not in txt)
