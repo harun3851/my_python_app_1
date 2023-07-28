05b457cf19f66d77a0a1bf183190af6bbd5d3d07
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-07-28 12:30:18 +0300

    format string

---
+++ format_string.py
@@ -0,0 +1,9 @@
+age = 36 
+txt = "my name is harun, and i am {}"
+print(txt.format(age))
+
+quantity=3
+itemno=567
+price=49.55
+myorder="i want {} pieces of item {} for {} dollars"
+print(myorder.format(quantity,itemno,price))
