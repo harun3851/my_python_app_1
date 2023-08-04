2ebe3c738d512e6bcb2c36d567694e08b97811b3
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-08-04 12:29:35 +0300

    pyton list

---
+++ list.py
@@ -0,0 +1,9 @@
+myList = ["apple","banana","cherry"]
+print(myList)
+print(len(myList))
+#access list
+thislist = ["apple", "banana", "cherry","orange","kiwi","melon","mango"]
+print(thislist[1])
+print(thislist[2:5])
+print(thislist[:4])
+print(thislist[2:])
