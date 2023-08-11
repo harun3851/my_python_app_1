8a052849dd1b6ca39ad634ddc097d3d05f5c27af
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-08-11 21:28:13 +0300

    python sets

---
+++ sets.py
@@ -0,0 +1,24 @@
+myset={"apple","banana","cherry","apple"}
+print(myset)
+
+
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False, False}
+
+print(set1)
+print(set2)
+print(set3)
+# in set it doesnt repeat same value
+
+thisset = {"apple", "banana", "cherry"}
+
+
+
+thisset.add("orange")
+
+print(thisset)
+
+thisset.remove("banana")
+
+print(thisset)
