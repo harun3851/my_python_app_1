d636aee81c70295f24400c4169660899c31a4c16
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-08-15 18:11:40 +0300

    dictionary

---
+++ dictioary.py
@@ -0,0 +1,16 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+print(len(thisdict))
+
+thisdicttype = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict["color"] = "red"
+print(type(thisdicttype))
+print(thisdict)
